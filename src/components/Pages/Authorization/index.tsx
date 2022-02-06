83f7d4c4bb6f21f69c314e09d9b4093b4b0a1d5e
olesiklesha <olesik736@gmail.com>
2022-02-06 11:57:52 +0300

    feat: complete footer and add container to authorization page

--- src/components/Pages/Authorization/index.tsx
+++ src/components/Pages/Authorization/index.tsx
@@ -1,10 +1,13 @@
+import { Container } from '@mui/material';
 import React from 'react';
 import { Footer } from '../..';
 
 function Authorization() {
   return (
     <>
-      <h1>Authorization</h1>
+      <Container sx={{ pt: 2, pb: 2, flexGrow: 1 }}>
+        <h1 className="title">Authorization</h1>
+      </Container>
       <Footer />
     </>
   );
