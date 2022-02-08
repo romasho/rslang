38eb346e76c146855f1428f837930f7607cf3a72
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-02-08 23:37:39 +0300

    feat: add tabs and sign in form

--- src/components/Pages/Authorization/index.tsx
+++ src/components/Pages/Authorization/index.tsx
@@ -1,14 +1,19 @@
-import { Grid, Paper, Box, Typography, TextField, Button } from '@mui/material';
+import { Grid, Paper, Box, Typography, TextField, Button, Tabs, Tab } from '@mui/material';
 import React from 'react';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import { Footer } from '../..';
 
 function Authorization() {
+  const [tabValue, setTabValue] = React.useState(1);
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+
   return (
     <>
-      <Grid container justifyContent="flex-end" sx={{
-        flexGrow: 1
-      }}
-      >
+      <Grid container justifyContent="flex-end" sx={{ flexGrow: 1  }}>
         <Grid
           item
           xs={false}
@@ -23,53 +28,113 @@ function Authorization() {
         />
         <Grid item xs={12} sm={6} md={4} component={Paper} elevation={6} sx={{ bgcolor: 'background.default' }}>
           <Box sx={{
-            my: 8,
-            mx: 4,
+            p: 4,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center'
           }}>
-            <Typography component='h4' variant='h4'>
-              Sign up
-            </Typography>
-            <Box component='form'>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="Name"
-                variant="standard"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                variant="standard"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                type="password"
-                id="password"
-                label="Password"
-                name="password"
-                autoComplete="current-password"
-                variant="standard"
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                fullWidth
-                sx={{ mt: 3, mb: 2 }}
-              >
+            <Tabs value={tabValue} indicatorColor="secondary" onChange={handleTabChange}>
+              <Tab label="Sign up" />
+              <Tab label="Sign in" />
+            </Tabs>
+
+            <Box
+              role="tabpanel"
+              sx={{
+              display: tabValue === 0? 'flex' : 'none',
+              flexDirection: 'column',
+              alignItems: 'center',
+              mt: 4
+            }}>
+              <PersonAddAltIcon color="secondary" sx={{ fontSize: 60, mb: 2 }} />
+              <Typography component='h4' variant='h4'>
                 Sign up
-              </Button>
+              </Typography>
+              <Box component='form' sx={{ maxWidth: 470 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Name"
+                  variant="standard"
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  variant="standard"
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  type="password"
+                  id="password"
+                  label="Password"
+                  name="password"
+                  autoComplete="current-password"
+                  variant="standard"
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign up
+                </Button>
+              </Box>
             </Box>
+
+            <Box
+              role="tabpanel"
+              sx={{
+                display: tabValue === 1? 'flex' : 'none',
+                flexDirection: 'column',
+                alignItems: 'center',
+                mt: 4
+              }}>
+              <LockOpenIcon color="secondary" sx={{ fontSize: 60, mb: 2 }} />
+              <Typography component='h4' variant='h4'>
+                Sign in
+              </Typography>
+              <Box component='form' sx={{ maxWidth: 470 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  variant="standard"
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  type="password"
+                  id="password"
+                  label="Password"
+                  name="password"
+                  autoComplete="current-password"
+                  variant="standard"
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign in
+                </Button>
+              </Box>
+            </Box>
+
           </Box>
         </Grid>
       </Grid>
