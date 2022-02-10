3082965e462418e6573683c8935b14ac785c6de9
olesiklesha <olesik736@gmail.com>
2022-02-10 21:40:49 +0300

    feat: start create main page

--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import { Footer } from '../..';
-
+// , Grid
 function Home() {
   return (
     <>
-      <Container sx={{ pt: 2, pb: 2, flexGrow: 1 }}>
-        <h1 className="title">RSLang is a handy app for learning English!</h1>
-
+      <Container maxWidth='lg' sx={{ pt: 2, pb: 2, flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+          {/* <Grid container spacing={2}>
+            <Grid
+              item
+              xs={false}
+              sm={6}
+              md={8}
+              sx={{
+                backgroundImage: 'https://pin.it/6okp0ZQ'
+              }}>adgs</Grid>
+            <Grid item>adsfgdn</Grid>
+          </Grid> */}
+          <h1 className="title">RSLang is a handy app for learning English!</h1>
+        </Box>
       </Container>
 
       <Footer />
