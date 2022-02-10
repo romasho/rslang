c26add7eb83f902ac7c595612c125f66b5981629
olesiklesha <olesik736@gmail.com>
2022-02-10 20:17:15 +0300

    feat: replace box shadow style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ const creators = ['olesiklesha', 'romasho', 'artyomkr']
 
 function Footer() {
   return (
-    <Box className='footer' sx={{ boxShadow: '0px -2px 4px -1px rgb(0 0 0 / 20%), 0px -4px 5px 0px rgb(0 0 0 / 14%), 0px -1px 10px 0px rgb(0 0 0 / 12%)' }}>
+    <Box className='footer'>
       <Container
         maxWidth='xl'
         sx={{
