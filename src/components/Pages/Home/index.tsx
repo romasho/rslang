a127e0f5539ce8d4b45878581cafb6104a429b50
olesiklesha <olesik736@gmail.com>
2022-02-11 15:48:40 +0300

    feat: continue create main page

--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import { Container, Box } from '@mui/material';
+import { Container, Box, Grid, Typography, Paper } from '@mui/material';
 import { Footer } from '../..';
-// , Grid
+
 function Home() {
   return (
     <>
-      <Container maxWidth='lg' sx={{ pt: 2, pb: 2, flexGrow: 1 }}>
+      <Container maxWidth='lg' disableGutters sx={{ pt: 2, pb: 2, flexGrow: 1 }}>
         <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-          {/* <Grid container spacing={2}>
+          <Grid container justifyContent='space-between' sx={{ m: 0, mt: 2 }}>
+
+            <Grid item xs={12} sm={7} md={7} >
+              <Typography variant='h3' gutterBottom component='h1' sx={{ fontWeight: 700 }}>LanguageHike</Typography>
+              <Typography variant='h5' component='h2'>Learn English step-by-step - just like hiking</Typography>
+            </Grid>
             <Grid
               item
-              xs={false}
-              sm={6}
-              md={8}
+              component={Paper}
+              xs={0}
+              sm={4}
               sx={{
-                backgroundImage: 'https://pin.it/6okp0ZQ'
-              }}>adgs</Grid>
-            <Grid item>adsfgdn</Grid>
-          </Grid> */}
+                p: 0,
+                height: '400px',
+                backgroundImage: 'url(https://i.pinimg.com/564x/ce/b3/50/ceb350fa618421659b23ed5602c4603e.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center 20%',
+                borderRadius: '20px',
+              }} />
+          </Grid>
+        </Box>
+        <Box>
           <h1 className="title">RSLang is a handy app for learning English!</h1>
+
         </Box>
       </Container>
+
 
       <Footer />
     </>
