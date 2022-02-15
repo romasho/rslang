5ce0db02756149680fc623a0c102e44a79c21c3f
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-02-15 23:03:21 +0300

    feat: improve game dialog design

--- src/components/GameDialog/index.tsx
+++ src/components/GameDialog/index.tsx
@@ -1,23 +1,40 @@
-import { Grid, Typography, Button, Paper, Stepper, Step, StepLabel } from '@mui/material';
+import { Grid, Typography, Button, Paper, IconButton } from '@mui/material';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import CloseIcon from '@mui/icons-material/Close';
+import CircleIcon from '@mui/icons-material/Circle';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import React from 'react';
 
 function GameDialog() {
   return (
-    <Grid container component={Paper} elevation={3} flexDirection='column' alignItems='center'>
-      <Grid container justifyContent='space-between'>
-        <MusicNoteIcon />
-        <Stepper>
-          <Step>
-            <StepLabel>``</StepLabel>
-          </Step>
-        </Stepper>
-        <CloseIcon />
+    <Grid container component={Paper} elevation={3} flexDirection='column' alignItems='center' sx={{ p: 2 }}>
+
+      <Grid container justifyContent='space-between' flexWrap='nowrap' sx={{ mb: 2 }}>
+        <IconButton sx={{ mr: '40px' }}>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <Grid container alignItems='center' justifyContent='center'>
+          <CircleIcon color="disabled" />
+          <CircleIcon color="disabled" />
+          <CircleIcon color="disabled" />
+        </Grid>
+        <IconButton>
+          <FullscreenIcon />
+        </IconButton>
+        <IconButton >
+          <CloseIcon />
+        </IconButton>
       </Grid>
+
+
+
       <Typography fontSize={32}>Word</Typography>
+      <IconButton>
+        <MusicNoteIcon />
+      </IconButton>
       <Typography fontSize={24}>Перевод</Typography>
-      <Grid container justifyContent='space-around'>
+      <Grid container justifyContent='space-around' sx={{ mt: 2 }}>
         <Button variant="contained" color='error'>Incorrect</Button>
         <Button variant="contained" color='success'>Correct</Button>
       </Grid>
