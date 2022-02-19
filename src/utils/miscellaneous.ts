9c95fdfab7c32c123c5e7afdb71ae93ab89c6ba9
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-02-19 18:48:28 +0300

    refactor: change export of playAudio() function

--- src/utils/miscellaneous.ts
+++ src/utils/miscellaneous.ts
@@ -1,8 +1,16 @@
 const audioPlayer = new Audio();
 
-export default function playAudio(url: string, volume: number = 0.1) {
+function playAudio(url: string, volume: number = 0.1) {
   audioPlayer.volume = volume;
   audioPlayer.src = url;
   audioPlayer.load();
   audioPlayer.play();
 }
+
+function random(min: number, max: number) {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+}
+
+export { playAudio, random }
